e3096a03a18a4602fe85950be5d9ad990e37d060
Stephen Li <stephen.li305@gmail.com>
2018-10-30 11:55:22 -0700

    add a map at oakland

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 console.log("hello, world");
 console.log("hello, patrick");
+
+// 
+// var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+//
+//
+// mapboxgl.accessToken = 'pk.eyJ1Ijoic3RlcGhlbmxpMzA1IiwiYSI6ImNqbncyaWR0ZzFsc2MzcW1rNWczbnVqeDYifQ.afmueMPaXRJ1f4XXcG0IgA';
+// var map = new mapboxgl.Map({
+// container: 'YOUR_CONTAINER_ELEMENT_ID',
+// style: 'mapbox://styles/mapbox/streets-v10'
+// });
