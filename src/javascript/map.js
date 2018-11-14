59153d17223b5c43c3eeabc3ce4c1b8f7f4e5fb2
Stephen Li <stephen.li305@gmail.com>
2018-11-14 14:32:18 -0800

    able to remove plane

--- src/javascript/map.js
+++ src/javascript/map.js
@@ -4,8 +4,6 @@ import City from './city.js'
 import Route from './route.js'
 // import { data } from '../API/api.js'
 
-
-
 // Atlantic
 const BOS = new City(-71.0589, 42.3601, "BOS")
 const BKN = new City(-73.9442, 40.6782, "BKN")
@@ -50,8 +48,6 @@ const SAS = new City(-98.4936, 29.4241, "SAS")
 
 const center = new City(-98.5795, 39.8283, "center" )
 
-
-
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3RlcGhlbmxpMzA1IiwiYSI6ImNqbncyaWR0ZzFsc2MzcW1rNWczbnVqeDYifQ.afmueMPaXRJ1f4XXcG0IgA';
 let map = new mapboxgl.Map({
     container: 'map',
@@ -59,11 +55,19 @@ let map = new mapboxgl.Map({
     center: center.pos,
     zoom: 4
 });
-map.addControl(new mapboxgl.NavigationControl())
 
+class Map {
+  constructor(){
+    this.steps = 100
+    this.flying = this.flying.bind(this)
+    this.createRoute = this.createRoute.bind(this)
+    this.drawRoute = this.drawRoute.bind(this)
+    this.drawAirplane = this.drawAirplane.bind(this)
+    this.annimateAirplane = this.annimateAirplane.bind(this)
 
+  }
 
-function createRoute(cities){
+ createRoute(cities){
   let result = []
   for (let i = 0; i < cities.length - 1; i++) {
     let city1 = cities[i]
@@ -74,12 +78,13 @@ function createRoute(cities){
   return result
 }
 
-function drawRoute(route){
-  // console.log(route);
+ drawRoute(route){
+
   map.addSource( `${route.getID()}-line`, {
     "type":"geojson",
     "data": route.route()
   }),
+  console.log(route),
 
   map.addLayer({
       "id": `${route.getID()}-line`,
@@ -92,24 +97,22 @@ function drawRoute(route){
   });
 }
 
-function clearRoute(route){
-  map.addSource( `${route.getID()}-line`, {
-    "type":"geojson",
-    "data": ""
-  }),
+//  clearRoute(route){
+//   map.addSource( `${route.getID()}-line`, {
+//     "type":"geojson",
+//     "data": ""
+//   }),
+//
+//   map.addLayer({
+//       "id": `${route.getID()}-line`,
+//       "source": `${route.getID()}-line`,
+//       "type": "",
+//       "paint": {
+//       }
+//   });
+// }
 
-  map.addLayer({
-      "id": `${route.getID()}-line`,
-      "source": `${route.getID()}-line`,
-      "type": "",
-      "paint": {
-      }
-  });
-}
-
-
-
-function drawAirplane(planeData){
+ drawAirplane(planeData){
   map.addSource( 'plane', {
     "type": "geojson",
     "data": planeData
@@ -129,40 +132,28 @@ function drawAirplane(planeData){
   });
 }
 
-
-const steps = 100
-
-// route.route().features[0].geometry.coordinates = route.arc()
-function annimateAirplane(planeObject, fullArc, counter = 0){
+ annimateAirplane(planeObject, fullArc, counter = 0){
 
   planeObject.features[0].geometry.coordinates = fullArc[counter];
   map.getSource('plane').setData(planeObject);
 
 
   planeObject.features[0].properties.bearing = turf.bearing(
-      turf.point(fullArc[counter >= steps ? counter - 1 : counter]),
-      turf.point(fullArc[counter >= steps ? counter : counter + 1])
+      turf.point(fullArc[counter >= this.steps ? counter - 1 : counter]),
+      turf.point(fullArc[counter >= this.steps ? counter : counter + 1])
   );
 
   if (counter < fullArc.length){
-    requestAnimationFrame(() => annimateAirplane(planeObject, fullArc, counter + 1))
+    requestAnimationFrame(() => this.annimateAirplane(planeObject, fullArc, counter + 1))
   }
 }
 
-const flyOAK = document.getElementById('OAK')
-
-
-function flying(){
-  // console.log(data);
-  // debugger
-  // console.log("hi");
+ flying(cities){
   // put in a list of cities in this array
-  let cities = [OAK,LA,PHX,OAK]
+  // let cities = [OAK,LA,PHX,OAK]
   let origin_city = cities[0].pos
-  // console.log(origin_city);
-  // let cities = [OAK,LA, PHX, HOU, DAL, OKC, MIA, PHI, MEM, ORL, CHA, BOS, NYC, TOR, CLE, MIN, UTA, POR, OAK]
-  let routes = createRoute(cities)
-  console.log(routes);
+  let routes = this.createRoute(cities)
+  // console.log(routes);
   let planeObject =
     {
       "type": "FeatureCollection",
@@ -177,36 +168,34 @@ function flying(){
 
   let fullArc = []
   for (var i = 0; i < routes.length; i++) {
-    drawRoute(routes[i])
-    fullArc.push(...routes[i].arc(steps))
+    this.drawRoute(routes[i])
+    fullArc.push(...routes[i].arc(this.steps))
   }
   planeObject.features[0].geometry.coordinates = routes[0].origin.pos
-  drawAirplane(planeObject)
-  annimateAirplane(planeObject, fullArc);
+  this.drawAirplane(planeObject)
+  this.annimateAirplane(planeObject, fullArc);
 }
 
-flyOAK.addEventListener('click', () => flying())
+ removeLayer(){
+   map.removeLayer('plane')
 
+   debugger
+   // map.
+ }
 
-
-
+}
 const remove = document.getElementById('Remove')
-
-function removeLayer(){
-  let cities = [OAK,LA,PHX,OAK]
-  let routes = createRoute(cities)
-  for (var i = 0; i < routes.length; i++) {
-    clearRoute(routes[i])
-  }
-  // console.log(routes);
-  // debugger
-  // clearRoute(routes)
-  // console.log('hi');
-}
-
-// remove.addEventListener('click', () => removeLayer())
-remove.addEventListener('click', () => removeLayer())
+remove.addEventListener('click', () => mapclass.removeLayer())
 
 
+
+const mapclass = new Map()
+const cities = [OAK,LA, PHX]
+const flyOAK = document.getElementById('OAK')
+flyOAK.addEventListener('click', () => mapclass.flying(cities))
+
+const lacities = [BOS, BKN, NYC]
+const flyLAL = document.getElementById('LAL')
+flyLAL.addEventListener('click', () => mapclass.flying(lacities))
 
 export default map
