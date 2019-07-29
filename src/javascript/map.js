f921df6907c24af413660cfe29785e076223751f
Stephen Li <stephen.li305@gmail.com>
2019-07-29 10:15:59 -0700

    refactored code

--- src/javascript/map.js
+++ src/javascript/map.js
@@ -45,6 +45,26 @@ const MEM = new City(-90.0490, 35.1495, "MEM")
 const NOP = new City(-90.0715, 29.9511, "NOP")
 const SAS = new City(-98.4936, 29.4241, "SAS")
 
+const route1 = [OAK, UTA, DEN, OAK]
+const route2 = [OAK, NYC, BKN, CHI, OAK]
+const route3 = [OAK, LA, OAK]
+const route4 = [OAK, HOU, DAL, SAS, OAK]
+const route5 = [OAK, TOR, DET, ATL, CLE, MIL, OAK]
+const route6 = [OAK, SAC, OAK]
+const route7 = [OAK, UTA, OAK]
+const route8 = [OAK, POR, PHX, OAK]
+const route9 = [OAK, DAL, DEN, OAK]
+const route10 = [OAK, LA, WAS, BOS, IND, OAK]
+const route11 = [OAK, PHX, OAK]
+const route12 = [OAK, POR, OAK]
+const route13 = [OAK, CHA, MIA, ORL, PHI, OAK]
+const route14 = [OAK, HOU, OKC, SAS, MIN, OAK]
+const route15 = [OAK, MEM, MIN, OAK]
+const route16 = [OAK, LA, OAK]
+const route17 = [OAK, NOP, MEM, OAK]
+const routes = [route1, route2, route3, route4, route5, route6, route7, route8, route9, route10, route11, route12, route13, route14, route15, route16, route17]
+
+
 const center = new City(-98.5795, 39.8283, "center" )
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3RlcGhlbmxpMzA1IiwiYSI6ImNqbncyaWR0ZzFsc2MzcW1rNWczbnVqeDYifQ.afmueMPaXRJ1f4XXcG0IgA';
@@ -174,106 +194,14 @@ class Map {
 }
 
  removeLayer(){
-  //  kill the call stack
-
-   // set to clear every route, will refactor in future
-   const route1 = [OAK,UTA, DEN, OAK]
    map.removeLayer('OAK')
-   for (var i = 0; i < route1.length - 1; i++) {
-     // if (map.getStyle().layers[`${route1[i].name + route1[i + 1].name}`]) {map.removeLayer(`${route1[i].name}${route1[i + 1].name}`)}
-     map.removeLayer(`${route1[i].name}${route1[i + 1].name}`) // refactor in future to get rid of minor errors
-     if (map.getStyle().sources[`${route1[i].name + route1[i + 1].name}`]) {map.removeSource(`${route1[i].name}${route1[i + 1].name}`)}
-   }
 
-   const route2 = [OAK, NYC, BKN, CHI, OAK]
-   for (var i = 0; i < route2.length - 1; i++) {
-     map.removeLayer(`${route2[i].name}${route2[i + 1].name}`)
-     if (map.getStyle().sources[`${route2[i].name + route2[i + 1].name}`]) {map.removeSource(`${route2[i].name}${route2[i + 1].name}`)}
+   for (let route of routes){     
+     for (var i = 0; i < route.length - 1; i++) {
+       map.removeLayer(`${route[i].name}${route[i + 1].name}`) 
+       if (map.getStyle().sources[`${route[i].name + route[i + 1].name}`]) {map.removeSource(`${route[i].name}${route[i + 1].name}`)}
+     }
    }
-
-   const route4 = [OAK, HOU, DAL, SAS, OAK]
-   for (var i = 0; i < route4.length - 1; i++) {
-     map.removeLayer(`${route4[i].name}${route4[i + 1].name}`)
-     if (map.getStyle().sources[`${route4[i].name + route4[i + 1].name}`]) {map.removeSource(`${route4[i].name}${route4[i + 1].name}`)}
-   }
-
-   const route5 = [OAK, TOR, DET, ATL, CLE, MIL, OAK]
-   for (var i = 0; i < route5.length - 1; i++) {
-     map.removeLayer(`${route5[i].name}${route5[i + 1].name}`)
-     if (map.getStyle().sources[`${route5[i].name + route5[i + 1].name}`]) {map.removeSource(`${route5[i].name}${route5[i + 1].name}`)}
-   }
-
-   const route6 = [OAK, SAC, OAK]
-   for (var i = 0; i < route6.length - 1; i++) {
-     map.removeLayer(`${route6[i].name}${route6[i + 1].name}`)
-     if (map.getStyle().sources[`${route6[i].name + route6[i + 1].name}`]) {map.removeSource(`${route6[i].name}${route6[i + 1].name}`)}
-   }
-
-   const route7 = [OAK, UTA, OAK]
-   for (var i = 0; i < route7.length - 1; i++) {
-     map.removeLayer(`${route7[i].name}${route7[i + 1].name}`)
-     if (map.getStyle().sources[`${route7[i].name + route7[i + 1].name}`]) {map.removeSource(`${route7[i].name}${route7[i + 1].name}`)}
-   }
-
-   const route8 = [OAK, POR, PHX, OAK]
-   for (var i = 0; i < route8.length - 1; i++) {
-     map.removeLayer(`${route8[i].name}${route8[i + 1].name}`)
-     if (map.getStyle().sources[`${route8[i].name + route8[i + 1].name}`]) {map.removeSource(`${route8[i].name}${route8[i + 1].name}`)}
-   }
-
-   const route9 = [OAK, DAL, DEN, OAK]
-   for (var i = 0; i < route9.length - 1; i++) {
-     map.removeLayer(`${route9[i].name}${route9[i + 1].name}`)
-     if (map.getStyle().sources[`${route9[i].name + route9[i + 1].name}`]) {map.removeSource(`${route9[i].name}${route9[i + 1].name}`)}
-   }
-
-   const route10 = [OAK, LA, WAS, BOS, IND, OAK]
-   for (var i = 0; i < route10.length - 1; i++) {
-     map.removeLayer(`${route10[i].name}${route10[i + 1].name}`)
-     if (map.getStyle().sources[`${route10[i].name + route10[i + 1].name}`]) {map.removeSource(`${route10[i].name}${route10[i + 1].name}`)}
-   }
-
-   const route11 = [OAK, PHX, OAK]
-   for (var i = 0; i < route11.length - 1; i++) {
-     map.removeLayer(`${route11[i].name}${route11[i + 1].name}`)
-     if (map.getStyle().sources[`${route11[i].name + route11[i + 1].name}`]) {map.removeSource(`${route11[i].name}${route11[i + 1].name}`)}
-   }
-
-   const route12 = [OAK, POR, OAK]
-   for (var i = 0; i < route12.length - 1; i++) {
-     map.removeLayer(`${route12[i].name}${route12[i + 1].name}`)
-     if (map.getStyle().sources[`${route12[i].name + route12[i + 1].name}`]) {map.removeSource(`${route12[i].name}${route12[i + 1].name}`)}
-   }
-
-   const route13 = [OAK, CHA, MIA, ORL, PHI, OAK]
-   for (var i = 0; i < route13.length - 1; i++) {
-     map.removeLayer(`${route13[i].name}${route13[i + 1].name}`)
-     if (map.getStyle().sources[`${route13[i].name + route13[i + 1].name}`]) {map.removeSource(`${route13[i].name}${route13[i + 1].name}`)}
-   }
-
-   const route14 = [OAK, HOU, OKC, SAS, MIN, OAK]
-   for (var i = 0; i < route14.length - 1; i++) {
-     map.removeLayer(`${route14[i].name}${route14[i + 1].name}`)
-     if (map.getStyle().sources[`${route14[i].name + route14[i + 1].name}`]) {map.removeSource(`${route14[i].name}${route14[i + 1].name}`)}
-   }
-
-   const route15 = [OAK, MEM, MIN, OAK]
-   for (var i = 0; i < route15.length - 1; i++) {
-     map.removeLayer(`${route15[i].name}${route15[i + 1].name}`)
-     if (map.getStyle().sources[`${route15[i].name + route15[i + 1].name}`]) {map.removeSource(`${route15[i].name}${route15[i + 1].name}`)}
-   }
-
-   const route16 = [OAK, LA, OAK]
-   for (var i = 0; i < route16.length - 1; i++) {
-     map.removeLayer(`${route16[i].name}${route16[i + 1].name}`)
-     if (map.getStyle().sources[`${route16[i].name + route16[i + 1].name}`]) {map.removeSource(`${route16[i].name}${route16[i + 1].name}`)}
-   }
-   const route17 = [OAK, NOP, MEM, OAK]
-   for (var i = 0; i < route17.length - 1; i++) {
-     map.removeLayer(`${route17[i].name}${route17[i + 1].name}`)
-     if (map.getStyle().sources[`${route17[i].name + route17[i + 1].name}`]) {map.removeSource(`${route17[i].name}${route17[i + 1].name}`)}
-   }
-
    setTimeout(map.removeSource('plane'),0)
  }
 }
@@ -281,96 +209,11 @@ class Map {
 
 const mapclass = new Map()
 
-//First Road Trip
-const route1 = [OAK,UTA, DEN, OAK]
-const fly1 = document.getElementById('OAK1')
-fly1.addEventListener('click', () => mapclass.flying(route1))
 
-//Second Road Trip
-const route2 = [OAK, NYC, BKN, CHI, OAK]
-const fly2 = document.getElementById('OAK2')
-fly2.addEventListener('click', () => mapclass.flying(route2))
-
-//Third Road Trip
-const route3 = [OAK, LA, OAK]
-const fly3 = document.getElementById('OAK3')
-fly3.addEventListener('click', () => mapclass.flying(route3))
-
-//Fourth Road Trip
-const route4 = [OAK, HOU, DAL, SAS, OAK]
-const fly4 = document.getElementById('OAK4')
-fly4.addEventListener('click', () => mapclass.flying(route4))
-
-//Fifth Road Trip
-const route5 = [OAK, TOR, DET, ATL, CLE, MIL, OAK]
-const fly5 = document.getElementById('OAK5')
-fly5.addEventListener('click', () => mapclass.flying(route5))
-
-//Sixth Road Trip
-const route6 = [OAK, SAC, OAK]
-const fly6 = document.getElementById('OAK6')
-fly6.addEventListener('click', () => mapclass.flying(route6))
-
-//Seventh Road Trip
-const route7 = [OAK, UTA, OAK]
-const fly7 = document.getElementById('OAK7')
-fly7.addEventListener('click', () => mapclass.flying(route7))
-
-//Eighth Road Trip
-const route8 = [OAK, POR, PHX, OAK]
-const fly8 = document.getElementById('OAK8')
-fly8.addEventListener('click', () => mapclass.flying(route8))
-
-//Ninth Road Trip
-const route9 = [OAK, DAL, DEN, OAK]
-const fly9 = document.getElementById('OAK9')
-fly9.addEventListener('click', () => mapclass.flying(route9))
-
-//Tenth Road Trip
-const route10 = [OAK, LA, WAS, BOS, IND, OAK]
-const fly10 = document.getElementById('OAK10')
-fly10.addEventListener('click', () => mapclass.flying(route10))
-
-//Eleventh Road Trip
-const route11 = [OAK, PHX, OAK]
-const fly11 = document.getElementById('OAK11')
-fly11.addEventListener('click', () => mapclass.flying(route11))
-
-//Twelveth Road Trip
-const route12 = [OAK, POR, OAK]
-const fly12 = document.getElementById('OAK12')
-fly12.addEventListener('click', () => mapclass.flying(route12))
-
-//Thirteenth Road Trip
-const route13 = [OAK, CHA, MIA, ORL, PHI, OAK]
-const fly13 = document.getElementById('OAK13')
-fly13.addEventListener('click', () => mapclass.flying(route13))
-
-//Fourteeth Road Trip
-const route14 = [OAK, HOU, OKC, SAS, MIN, OAK]
-const fly14 = document.getElementById('OAK14')
-fly14.addEventListener('click', () => mapclass.flying(route14))
-
-//Fifteen Road Trip
-const route15 = [OAK, MEM, MIN, OAK]
-const fly15 = document.getElementById('OAK15')
-fly15.addEventListener('click', () => mapclass.flying(route15))
-
-//Sixteen Road Trip
-const route16 = [OAK, LA, OAK]
-const fly16 = document.getElementById('OAK16')
-fly16.addEventListener('click', () => mapclass.flying(route16))
-
-//Seventeenth Road Trip
-const route17 = [OAK, NOP, MEM, OAK]
-const fly17 = document.getElementById('OAK17')
-fly17.addEventListener('click', () => mapclass.flying(route17))
-
-
-
-
-
-
-
+// adding the actions with the buttons
+for(let i = 1; i < 18 ;i++){
+  let route = routes[i - 1]; 
+  document.getElementById("OAK" + i).addEventListener('click', () => mapclass.flying(route))
+}
 
 export default map
